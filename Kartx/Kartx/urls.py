448b3ecf6b991259bb64636b80a3c1191168dad9
Febin <febinvinod@gmail.com>
2025-01-08 09:45:04 +0530

    updated

--- Kartx/Kartx/urls.py
+++ Kartx/Kartx/urls.py
@@ -20,6 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('catalog.urls')),
-    path('', include('Kartx_cart.urls')),
+    path('', include('kartx_cart.urls')),
     path('api/accounts/', include('accounts.urls')),
 ]
