f066c7d4d993879db4b02cd65b2f5d0d92cb134d
BenJoseJoseph2 <benjosejoseph555@gmail.com>
2025-01-08 14:44:46 +0530

    Notification-Sys

---
+++ Kartx/notification_system/models.py
@@ -0,0 +1,15 @@
+# notifications/models.py
+payment_data = [
+    {
+        "payment_id": "P12345",
+        "user_name": "John",
+        "email": "benjosejoseph555@gmail.com",
+        "order_id": "ORD123456"
+    },
+    {
+        "payment_id": "P12346",
+        "user_name": "Smith",
+        "email": "benjosejoseph555@gmail.com",
+        "order_id": "ORD123457"
+    }
+]
